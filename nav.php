7e0b9f649363c9d6cb5563447810e48ed1221da2
cuyocosmico <cuyocosmico@gmail.com>
2020-11-24 20:28:04 -0600

    boton arreglado

--- nav.php
+++ nav.php
@@ -16,7 +16,7 @@
             ?>
           </div>
         <nav class="navbar navbar-expand-lg navbar-light bg-success ">
-            <button class="btn btn-success" type="submit"><img class="icono1" src="img/menu.png" alt="" onclick="openNav()"></button>
+            <button class="btn btn-success" onclick="openNav()" type="submit"><img class="icono1" src="img/menu.png" alt="" ></button>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
